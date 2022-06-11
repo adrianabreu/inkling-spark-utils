6a4879b70c52ceec9d10441d5375d98006a3b39c
Adrian Abreu <aabreu@countergrogwise.com>
2022-06-11 18:13:49 +0200

    Publishing package to pipy

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spark - Utils
+
+This poetry aims to compile small utilities I tend to use on my daily job as well as to explore some alternative solutions for common problems.
---
+++ tests/test_dataframe.py
@@ -0,0 +1,71 @@
+import pytest
+from pyspark.sql import SparkSession
+from pyspark.sql.types import (StructType, StructField, LongType, IntegerType, StringType)
+from spark_utils.dataframe import complete_dataframe, select_schema_columns, cast_dataframe
+
+
+@pytest.fixture(scope='session')
+def spark():
+    return SparkSession.builder \
+      .master("local") \
+      .appName("chispa") \
+      .getOrCreate()
+
+
+def test_complete_dataframe(spark):
+    mockInput = spark.createDataFrame(
+        [
+            (1, 'foo'), 
+            (2, 'bar'),
+        ],
+        ['id', 'txt'] 
+    )
+
+    mockSchema = StructType([
+    StructField("id", LongType()),
+    StructField("txt", StringType()),
+    StructField("age", LongType())
+    ])
+
+    expected_schema = [("id", LongType()), ("txt", StringType()) , ("age", LongType())]
+
+    actual = [(x.name, x.dataType) for x in complete_dataframe(mockInput, mockSchema).schema.fields]
+    
+    assert expected_schema == actual 
+
+def test_cast_dataframe(spark):
+    mockInput = spark.createDataFrame(
+        [
+            (1, 'foo'), 
+            (2, 'bar'),
+        ],
+        ['id', 'txt'] 
+    )
+
+    mockSchema = StructType([
+    StructField("id", IntegerType()),
+    StructField("txt", StringType())
+    ])
+
+    expected_schema = [("id", IntegerType()), ("txt", StringType())]
+
+    actual = [(x.name, x.dataType) for x in cast_dataframe(mockInput, mockSchema).schema.fields]
+    
+    assert expected_schema == actual 
+
+def test_select_schema_cols(spark):
+    mockInput = spark.createDataFrame(
+        [
+            (1, 'foo','bar'), 
+            (2, 'bar','baz'),
+        ],
+        ['id', 'txt','name'] 
+    )
+
+    target_schema = StructType([
+    StructField("id", IntegerType())
+    ])
+
+    actual = [(x.name) for x in select_schema_columns(mockInput, target_schema).schema.fields]
+    
+    assert [x.name for x in target_schema.fields] == actual 
